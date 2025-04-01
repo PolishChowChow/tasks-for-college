ac79db90401941df2c76fc5ae3827edfe16f2ca6
PolishChowChow <polishchowchow@gmail.com>
2025-04-01 19:54:05 +0200

    fix: fixed task 8

--- zadanie_8/main.cpp
+++ zadanie_8/main.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
-#include"include/Prostopadloscian.h"
+#include"include/Szescian.h"
 #include"include/Walec.h"
 #include"include/Figura.h"
 
 int main(){
     Figura* wsk;
-    wsk = new Prostopadloscian(2, 10);
-    std::cout<<"Pole prostopadloscianu: "<<wsk->oblicz_pole()<<std::endl;
+    wsk = new Szescian(5);
+    std::cout<<"Pole szescianu: "<<wsk->oblicz_pole()<<std::endl;
     wsk = new Walec(2, 10);
     std::cout<<"Pole walca: "<<wsk->oblicz_pole();
     // delete wsk;
